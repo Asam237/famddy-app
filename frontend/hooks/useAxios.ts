cc334526107a2718e07764527fafa7c51dce2ca2
Abba Sali <abbasaliaboubakar@gmail.com>
2023-10-20 08:18:39 +0100

    update endpoint

--- frontend/hooks/useAxios.ts
+++ frontend/hooks/useAxios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const baseURL = "http://localhost:3010/";
-const baseURL = "https://famddy-api.abbasali.cm/";
+const baseURL = "https://fdy.abbasali.cm/";
 
 export const api = axios.create({
     baseURL,
